3d27d67e87c27ac897a4c2bab1615be65585f444
davidson2521190431022@aluno.eteprc.faetec.rj.gov.br <thcunhas01@gmail.com>
2026-08-27 14:49:42 -0300

    first commit

--- subir.cmd
+++ subir.cmd
@@ -8,3 +8,5 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:davidson2521190431022-jpg/aula6.git
 git push -u origin main
+
+start https://github.com/davidson2521190431022-jpg/aula6
